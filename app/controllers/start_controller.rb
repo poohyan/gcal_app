c5b0c370cc64501b93165f513994e5a267f9b7fa
Tsuyoshi Ota <2yoshi.ota@gmail.com>
2014-05-31 05:40:25 +0900

    expired date比較の方法変更

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -11,7 +11,7 @@ skip_filter :oauth2
     if Rails.env == 'development'
       TokenPair.delete_all("datetime(expires_in + issued_at)  < datetime('now', 'utc')")
   	else
-      TokenPair.delete_all("expires_in + issued_at  < extract(epoch from timestamp ’CURRENT_TIMESTAMP') ")
+      TokenPair.delete_all("expires_in + issued_at  < date_part('epoch',CURRENT_TIMESTAMP) ")
     end
   end
 
